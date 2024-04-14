09f3c83ad5dd3ef1b8b643a2e17bd12667c9c83b
Arpit319-byte <arpitchauhanupal@gmail.com>
2024-04-14 16:51:00 +0530

    intial commit

---
+++ UberApplication/src/test/java/com/example/UberApplication/ReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UberApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
