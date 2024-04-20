83bb71d89a75e9353fc07268d0507aca4fdc48d3
Arpit319-byte <arpitchauhanupal@gmail.com>
2024-04-20 18:30:42 +0530

    Added first model class

---
+++ UberApplication/src/main/java/com/example/UberApplication/model/ReviewService.java
@@ -0,0 +1,14 @@
+package com.example.UberApplication.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class ReviewService {
+
+    @Id
+    private Long id;
+
+
+
+}
