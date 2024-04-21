63a7bd138929a3f4339ab4e4d4eb9036bb348753
Arpit319-byte <arpitchauhanupal@gmail.com>
2024-04-21 20:06:05 +0530

    Added lombok functionality in review model

--- UberApplication/src/main/java/com/example/UberApplication/model/Review.java
+++ UberApplication/src/main/java/com/example/UberApplication/model/Review.java
@@ -1,6 +1,7 @@
 package com.example.UberApplication.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -8,6 +9,11 @@ import org.springframework.data.annotation.LastModifiedDate;
 import java.util.Date;
 
 @Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "ReviewTable")
 public class Review {
     @Id
@@ -19,10 +25,12 @@ public class Review {
 
     private String rating;
 
+    @Column(nullable = false)
     @Temporal(TemporalType.TIMESTAMP) // It is used for setting the time/date/date.time
     @CreatedDate // This tells the spring to only handle it at the creation of object
     private Date createdOn;
 
+    @Column(nullable = false)
     @Temporal(TemporalType.TIMESTAMP)
     @LastModifiedDate // This tells the spring to only handle it at the update of object
     private Date updatedOn;
